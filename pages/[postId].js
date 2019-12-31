0d784fa799bb30aa2e936e354b8dcfe8a261c669
Oğuz Kağan Yağlıoğlu <oguzkagan05@gmail.com>
2019-12-31 16:39:53 +0300

    Upload

--- pages/[postId].js
+++ pages/[postId].js
@@ -78,7 +78,7 @@ const BlogPost = ({ post }) => (
 
 BlogPost.getInitialProps = async ({ req, query }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch(`http://localhost:3000/api/post/${query.postId}`);
+  const res = await fetch(`http://perfectwithme.herokuapp.com/api/post/${query.postId}`);
   const json = await res.json();
   return { post: json.post };
 };
