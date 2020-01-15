29b035f831ffc8a658d6ce39b471accbb0558e45
Oğuz Kağan Yağlıoğlu <oguzkagan05@gmail.com>
2020-01-15 14:31:12 +0300

    Update auth.router.js

--- server/routes/auth/auth.router.js
+++ server/routes/auth/auth.router.js
@@ -8,42 +8,38 @@ const route = () => {
   const router = new express.Router();
 
 
-  // router.route(`/login`).post((req, res) => {
-  //   const { email, password } = req.body;
-  //   const passwordHashed = crypto
-  //     .createHmac("sha512", process.env.PASS_SECRET)
-  //     .update(password)
-  //     .digest("hex");
-
-  //   User.findOne({ email: email }).then(user => {
-  //     if (!user) {
-  //       res.send({
-  //         status: false,
-  //         message: "kullanıcı bulunamadı"
-  //       });
-  //     } else {
-  //       if (user.password === passwordHashed) {
-  //         const token = jwt.sign({ userId: user._id, admin: user.admin }, process.env.JWT_SECRET);
-  //         req.session.userToken = token;
-  //         req.session.useremail = email;
-  //         // req.session.user.lastName = user.lastName;
-  //         // req.session.user.firstName = user.firstName;
-  //         res.send({
-  //           status: true,
-  //           token: token
-  //         });
-  //       } else {
-  //         res.send({
-  //           status: false,
-  //           message: "kullanıcı bulunamadı"
-  //         });
-  //       }
-  //     }
-  //   });
-  // });
-
   router.route(`/login`).post((req, res) => {
-    res.send("Hiiiiiii");
+    const { email, password } = req.body;
+    const passwordHashed = crypto
+      .createHmac("sha512", process.env.PASS_SECRET)
+      .update(password)
+      .digest("hex");
+
+    User.findOne({ email: email }).then(user => {
+      if (!user) {
+        res.send({
+          status: false,
+          message: "kullanıcı bulunamadı"
+        });
+      } else {
+        if (user.password === passwordHashed) {
+          const token = jwt.sign({ userId: user._id, admin: user.admin }, process.env.JWT_SECRET);
+          req.session.userToken = token;
+          req.session.useremail = email;
+          // req.session.user.lastName = user.lastName;
+          // req.session.user.firstName = user.firstName;
+          res.send({
+            status: true,
+            token: token
+          });
+        } else {
+          res.send({
+            status: false,
+            message: "kullanıcı bulunamadı"
+          });
+        }
+      }
+    });
   });
 
   router.route(`/register`).post((req, res) => {
@@ -61,14 +57,14 @@ const route = () => {
       dateCreated: new Date(),
       dateModified: new Date()
     });
-    // newUser.save().then(
-    //   data => {
-    //     res.send({ status: true, user: data });
-    //   },
-    //   err => {
-    //     res.send({ status: false, error: err });
-    //   }
-    // );
+    newUser.save().then(
+      data => {
+        res.send({ status: true, user: data });
+      },
+      err => {
+        res.send({ status: false, error: err });
+      }
+    );
     res.send(passwordHashed);
   });
 
